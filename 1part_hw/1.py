e88eb76ebcbdb74d251089744812dfbffe848336
CyberGatos <slavikstine@gmail.com>
2025-07-09 01:38:37 +0300

    done 1 part of HW

---
+++ 1part_hw/1.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import re
+
+path = Path("/Users/cyber/Desktop/VS PROJECTS/goit-pycore-hw-04/1part_hw/my specialists.txt")
+
+def total_salary(path):
+    try:
+        with open(path, "r") as salary:
+            salary_list = []
+            for sal in salary:
+                digits = re.findall(r"\d+", sal)
+                digit = int("".join(digits))
+                salary_list.append(digit)
+            total = sum(salary_list)
+            averege = sum(salary_list) / len(salary_list)
+        return total, int(averege)
+    except:
+        return None
+
+if total_salary(path) is None:
+    print("Перевірте коректність файлу")
+else:
+    total, average = total_salary(path)
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+            
